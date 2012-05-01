8a979135d793f6f4309627ae3206c36a66cfed62
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/WikiWorkbenchContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/WikiWorkbenchContrib/build.pl
+++ lib/Foswiki/Contrib/WikiWorkbenchContrib/build.pl
@@ -1,12 +1,12 @@
 #!/usr/bin/perl -w
 
 BEGIN {
-  foreach my $pc (split(/:/, $ENV{FOSWIKI_LIBS})) {
-    unshift @INC, $pc;
-  }
+    foreach my $pc ( split( /:/, $ENV{FOSWIKI_LIBS} ) ) {
+        unshift @INC, $pc;
+    }
 }
 
 use Foswiki::Contrib::Build;
 $build = new Foswiki::Contrib::Build('WikiWorkbenchContrib');
-$build->build($build->{target});
+$build->build( $build->{target} );
 
