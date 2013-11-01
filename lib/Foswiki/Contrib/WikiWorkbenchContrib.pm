34fc635e317df4ccf4bbb83ae6355473d706b24a
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2013-11-01 12:14:32 +0000

    Item12621: multiple improvements
       * simplified application template
       * hide H1 button in NatEditPlugin when editing a <nop>WikiTopic as a H1 is generated by the view template already
       * better support for NOAUTOLINK
       * display quality warnings on the view, not in the editor
       * add support for explicit thumbnail image attachments (those that have a "t" attribute)
    
    
    
    git-svn-id: http://svn.foswiki.org/trunk/WikiWorkbenchContrib@17024 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/WikiWorkbenchContrib.pm
+++ lib/Foswiki/Contrib/WikiWorkbenchContrib.pm
@@ -16,8 +16,8 @@
 
 package Foswiki::Contrib::WikiWorkbenchContrib;
 
-our $VERSION = '1.11';
-our $RELEASE = '1.11';
+our $VERSION = '1.12';
+our $RELEASE = '1.12';
 our $SHORTDESCRIPTION = 'Framework for <nop>WikiApplications';
 
 1;
